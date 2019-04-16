3aa6c2954763e4c6aab0b7acaf5e119620290f3d
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2019-04-16 15:15:09 -0500

    update

--- InsecureVersion/GlobalConstants.py
+++ InsecureVersion/GlobalConstants.py
@@ -12,18 +12,8 @@ MANAGER_RSP_TOPIC = 'CustomerOrderRsp'
 MQTT_ADDR = ''
 MQTT_PRT = 1883
 
-HTTP_HEADER = {'Content-Type': 'application/json'}
-HTTP_CRT = ''
-
 MANAGER_ADDR = ''
 MANAGER_PRT = 5000
-
-USERNAME = ''
-PASSWORD = ''
-
-TLS_CERT = ''
-
-MESSAGE_DECRYPT_KEY = ''
 
 # ##############################
 # DynamoDB Constants
